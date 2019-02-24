24fbdb703b8f5fbc1b26356d6cbfcea160c8944e
Michael Wuertinger <michael@wuertinger.de>
2019-02-24 21:19:11 +0100

    Listing sub-volumes

---
+++ main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os/exec"
+	"sort"
+	"strings"
+)
+
+func main() {
+	remote := destination{
+		host: "target-host",
+		port: 10022,
+	}
+
+	subVolumesLocal, err := getSubVolumes(localhost, "/mnt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	subVolumesRemote, err := getSubVolumes(remote, "/mnt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Should already be sorted but just to be sure
+	sort.Strings(subVolumesLocal)
+	sort.Strings(subVolumesRemote)
+
+	fmt.Println("local:")
+	for _, subvolume := range subVolumesLocal {
+		fmt.Println(subvolume)
+	}
+	fmt.Println("\nremote:")
+	for _, subvolume := range subVolumesRemote {
+		fmt.Println(subvolume)
+	}
+
+	// mostRecentRemote :=
+}
+
+func getSubVolumes(d destination, mountPoint string) ([]string, error) {
+	return execPipe(d, parseSubVolumes, "btrfs", "subvolume", "list", mountPoint)
+}
+
+// parser parses the output of a process and returns the result as a slice of strings.
+type parser func(r io.Reader) ([]string, error)
+
+// destination determines where a command is to be executed.
+type destination struct {
+	host string
+	port int
+}
+
+// localhost is a special destination for running commands locally
+var localhost destination
+
+// execPipe runs the specified cmd at destination d and parses the result with p before returning it.
+func execPipe(d destination, p parser, cmd ... string) ([]string, error) {
+	var c *exec.Cmd
+	if d == localhost {
+		c = exec.Command(cmd[0], cmd[1:]...)
+	} else {
+		c = exec.Command("ssh", fmt.Sprintf("-p%d", d.port), d.host, strings.Join(cmd, " "))
+	}
+
+	pipe, err := c.StdoutPipe()
+	if err != nil {
+		return nil, fmt.Errorf("execPipe: %v", err)
+	}
+	defer pipe.Close()
+	if err := c.Start(); err != nil {
+		return nil, fmt.Errorf("execPipe: %v", err)
+	}
+
+	res, err := p(pipe)
+	if err != nil {
+		return nil, fmt.Errorf("execPipe: %v", err)
+	}
+
+	if err := c.Wait(); err != nil {
+		return nil, fmt.Errorf("execPipe: %v", err)
+	}
+
+	return res, nil
+}
+
+// parseSubVolumes extracts the sub-volume names from the "btrfs subvolume list" command.
+func parseSubVolumes(r io.Reader) ([]string, error) {
+	br := bufio.NewReader(r)
+	var names []string
+	for {
+		line, err := br.ReadBytes('\n')
+		if err == io.EOF {
+			return names, nil
+		}
+		tokens := strings.Split(string(line), " ")
+		if len(tokens) != 9 {
+			return nil, fmt.Errorf("parseSubVolumes: unexpected btrfs output: %s", line)
+		}
+		names = append(names, strings.TrimRight(tokens[8], "\n"))
+	}
+
+
+	return names, nil
+}
---
+++ main_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestParseSubvolumes(t *testing.T) {
+	btrfsOutput := `ID 6986 gen 23961 top level 5 path snapshot/2019-01-10_03-00
+ID 6988 gen 23968 top level 5 path snapshot/2019-01-11_03-00
+ID 6989 gen 23981 top level 5 path snapshot/2019-01-12_03-00
+ID 6990 gen 24002 top level 5 path snapshot/2019-01-13_03-00
+ID 6992 gen 24055 top level 5 path snapshot/2019-01-14_03-00
+ID 6993 gen 24105 top level 5 path snapshot/2019-01-15_03-00
+ID 7562 gen 24525 top level 5 path snapshot/2019-01-16_03-00
+ID 7564 gen 24529 top level 5 path snapshot/2019-01-17_03-00
+ID 7565 gen 24695 top level 5 path snapshot/2019-01-18_03-00
+ID 7566 gen 24776 top level 5 path snapshot/2019-01-19_03-00
+ID 7567 gen 24791 top level 5 path snapshot/2019-01-20_03-00
+ID 7568 gen 24803 top level 5 path snapshot/2019-01-21_03-00
+ID 7569 gen 24809 top level 5 path snapshot/2019-01-22_03-00
+ID 7570 gen 24823 top level 5 path snapshot/2019-01-23_03-00
+ID 7571 gen 24828 top level 5 path snapshot/2019-01-24_03-00
+ID 7572 gen 24829 top level 5 path snapshot/2019-01-25_03-00
+ID 7573 gen 24830 top level 5 path snapshot/2019-01-26_03-00
+ID 7574 gen 24831 top level 5 path snapshot/2019-01-27_03-00
+ID 7575 gen 24899 top level 5 path snapshot/2019-01-28_03-00
+ID 7576 gen 24932 top level 5 path snapshot/2019-01-29_03-00
+ID 7577 gen 24965 top level 5 path snapshot/2019-01-30_03-00
+ID 7578 gen 24969 top level 5 path snapshot/2019-01-31_03-00
+`
+
+	expectedSubvolumes := []string{"snapshot/2019-01-10_03-00", "snapshot/2019-01-11_03-00", "snapshot/2019-01-12_03-00", "snapshot/2019-01-13_03-00", "snapshot/2019-01-14_03-00", "snapshot/2019-01-15_03-00", "snapshot/2019-01-16_03-00", "snapshot/2019-01-17_03-00", "snapshot/2019-01-18_03-00", "snapshot/2019-01-19_03-00", "snapshot/2019-01-20_03-00", "snapshot/2019-01-21_03-00", "snapshot/2019-01-22_03-00", "snapshot/2019-01-23_03-00", "snapshot/2019-01-24_03-00", "snapshot/2019-01-25_03-00", "snapshot/2019-01-26_03-00", "snapshot/2019-01-27_03-00", "snapshot/2019-01-28_03-00", "snapshot/2019-01-29_03-00", "snapshot/2019-01-30_03-00", "snapshot/2019-01-31_03-00"}
+
+	subvolumes, err := parseSubVolumes(strings.NewReader(btrfsOutput))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(subvolumes)
+
+	if len(subvolumes) != len(expectedSubvolumes) {
+		t.Fatal("length differs")
+	}
+
+	for i := range expectedSubvolumes {
+		if subvolumes[i] != expectedSubvolumes[i] {
+			t.Fatalf("result differs at position %d: '%s' != '%s'", i, subvolumes[i], expectedSubvolumes[i])
+		}
+	}
+}
